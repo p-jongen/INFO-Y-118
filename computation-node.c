661103dacf318657f7329b170ed179176690a163
Emeric Demeulenere <emericemeu@hotmail.com>
2022-05-10 15:21:39 +0200

    v2

--- computation-node.c
+++ computation-node.c
@@ -3,14 +3,25 @@
 #include "net/nullnet/nullnet.h"
 #include <string.h>
 #include <stdio.h> /* For printf() */
-#include "node.h"
 
 /* Log configuration */
 #include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
 
-uint16_t
+/* Configuration */
+#define SEND_INTERVAL (8 * CLOCK_SECOND)
 
-//Receiving with NullNet
+#if MAC_CONF_WITH_TSCH
+#include "net/mac/tsch/tsch.h"
+static linkaddr_t coordinator_addr =  {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
+#endif /* MAC_CONF_WITH_TSCH */
+
+/*---------------------------------------------------------------------------*/
+PROCESS(nullnet_example_process, "NullNet broadcast example");
+AUTOSTART_PROCESSES(&nullnet_example_process);
+
+/*---------------------------------------------------------------------------*/
 void input_callback(const void *data, uint16_t len,
                     const linkaddr_t *src, const linkaddr_t *dest)
 {
@@ -22,51 +33,37 @@ void input_callback(const void *data, uint16_t len,
         LOG_INFO_("\n");
     }
 }
-
-PROCESS(computation_process, "Computation node");
-AUTOSTART_PROCESSES(&computation_process);
-
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(computation_process, ev, data)
+PROCESS_THREAD(nullnet_example_process, ev, data)
 {
-  static struct etimer periodic_timer;
-  static unsigned count = 0;
+static struct etimer periodic_timer;
+static unsigned count = 0;
 
-  PROCESS_BEGIN();
+PROCESS_BEGIN();
 
-    #if MAC_CONF_WITH_TSCH
-    tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
-    #endif /* MAC_CONF_WITH_TSCH */
+#if MAC_CONF_WITH_TSCH
+tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
+#endif /* MAC_CONF_WITH_TSCH */
 
-  /* Initialize NullNet */
-  /*
-  nullnet_buf = (uint8_t *)&count;
-  nullnet_len = sizeof(count);
-  nullnet_set_input_callback(input_callback);
-*/
-  //Receiving with NullNet (set a event)
-  nullnet_set_input_callback(input_callback);
-  while(1){
+/* Initialize NullNet */
+nullnet_buf = (uint8_t *)&count;
+nullnet_len = sizeof(count);
+nullnet_set_input_callback(input_callback);
 
+etimer_set(&periodic_timer, SEND_INTERVAL);
+while(1) {
+PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+LOG_INFO("Sending %u to ", count);
+LOG_INFO_LLADDR(NULL);
+LOG_INFO_("\n");
 
-  }
+memcpy(nullnet_buf, &count, sizeof(count));
+nullnet_len = sizeof(count);
 
-  /*
-  if(!linkaddr_cmp(&dest_addr, &linkaddr_node_addr)) {
-    etimer_set(&periodic_timer, SEND_INTERVAL);
-    while(1) {
-      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-      LOG_INFO("Sending %u to ", count);
-      LOG_INFO_LLADDR(&dest_addr);
-      LOG_INFO_("\n");
-
-      NETSTACK_NETWORK.output(&dest_addr);
-      count++;
-      etimer_reset(&periodic_timer);
-    }
-  }
-*/
-
-  PROCESS_END();
+NETSTACK_NETWORK.output(NULL);
+count++;
+etimer_reset(&periodic_timer);
 }
-/*---------------------------------------------------------------------------*/
+
+PROCESS_END();
+}
