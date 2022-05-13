d8135d8fb8aa8625d01aacec5e036f321e7cf4d1
hamze10 <hamzamahmoudi10@gmail.com>
2022-05-13 10:28:49 +0200

    wip

--- border-node.c
+++ border-node.c
@@ -1,3 +1,4 @@
+
 #include "contiki.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
@@ -13,93 +14,57 @@
 /* Configuration */
 #define SEND_INTERVAL (8 * CLOCK_SECOND)
 
-#if MAC_CONF_WITH_TSCH
-#include "net/mac/tsch/tsch.h"
-static linkaddr_t coordinator_addr =  {{ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }};
-#endif /* MAC_CONF_WITH_TSCH */
-
 
 /*---------------------------------------------------------------------------*/
-PROCESS(border_process_init, "border_process");
-//PROCESS(border_process, "border_process");
-AUTOSTART_PROCESSES(&border_process_init /*,&border_process*/ );
+PROCESS(nullnet_example_process, "NullNet broadcast example");
+AUTOSTART_PROCESSES(&nullnet_example_process);
 
 /*---------------------------------------------------------------------------*/
 void input_callback(const void *data, uint16_t len,
                     const linkaddr_t *src, const linkaddr_t *dest)
 {
-    LOG_INFO("Received (outside if in border node)");
-    if(len == sizeof(unsigned)) {
+    LOG_INFO("Receiving Border out IF\n");
+    LOG_INFO_("\n");
+    if(len == sizeof(int)) {
+
+        LOG_INFO("Receiving Border in IF INT\n");
+        LOG_INFO_("\n");
         unsigned count;
         memcpy(&count, data, sizeof(count));
+        /*
         LOG_INFO("Received %u from ", count);
         LOG_INFO_LLADDR(src);
         LOG_INFO_("\n");
+         */
+    }else if (len == sizeof(uint8_t)){
+        LOG_INFO("Receiving Border in IF UINT8_t\n");
+        LOG_INFO_("\n");
+    }else if (len == sizeof(uint16_t)){
+        LOG_INFO("Receiving Border in IF UINT16_t\n");
+        LOG_INFO_("\n");
+    }else if (len == sizeof(int[64])){
+        LOG_INFO("Receiving Border in IF INT[64]\n");
+        LOG_INFO_("\n");
     }
 }
-
 /*---------------------------------------------------------------------------*/
-
-PROCESS_THREAD(border_process_init, ev, data)
-{
-    //static struct etimer periodic_timer;
-
-    uint8_t  border_header[64] = {[0 ... 63] = -1};
-    static short rank = 1;
-    PROCESS_BEGIN();
-
-    //INIT
-    broadcastMsg msgPrep;
-    msgPrep.rank = rank;
-    msgPrep.typeMsg = 1;
-    constructHeader(border_header, msgPrep);
-
-    #if MAC_CONF_WITH_TSCH
-    tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
-    #endif /* MAC_CONF_WITH_TSCH */
-
-    LOG_INFO("FIRST THREAD");
-    LOG_INFO("\n");
-    printf("first thread en print");
-
-    nullnet_buf = (uint8_t *)&border_header;
-    nullnet_len = sizeof(border_header);
-    nullnet_set_input_callback(input_callback);
-    NETSTACK_NETWORK.output(NULL);
-    
-    PROCESS_END();
-}
-
-/*---------------------------------------------------------------------------*/
-/*
-PROCESS_THREAD(border_process, ev, data){
+PROCESS_THREAD(nullnet_example_process, ev, data){
     static struct etimer periodic_timer;
-    static unsigned count = 0;
-    //static short rank = 1;
 
     PROCESS_BEGIN();
 
-    nullnet_buf = (uint8_t *)&count;
-    nullnet_len = sizeof(count);
+    nullnet_set_input_callback(input_callback); //LISTENER packet
 
-    //nullnet_set_input_callback(input_callback);
-
-    LOG_INFO("SECOND THREAD");
-    LOG_INFO("\n");
     etimer_set(&periodic_timer, SEND_INTERVAL);
     while(1) {
-            PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-        LOG_INFO("Sending %u to ", count);
-        LOG_INFO_LLADDR(NULL);
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+        LOG_INFO("SENDING BORDER");
         LOG_INFO_("\n");
 
-        memcpy(nullnet_buf, &count, sizeof(count));
-        nullnet_len = sizeof(count);
-
         NETSTACK_NETWORK.output(NULL);
-        count++;
+
         etimer_reset(&periodic_timer);
     }
+
     PROCESS_END();
 }
-*/
