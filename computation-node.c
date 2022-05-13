70b01374dffddf046d8878c7aaf65e1eda206575
Emeric Demeulenere <emericemeu@hotmail.com>
2022-05-13 14:54:59 +0200

    v4.2

--- computation-node.c
+++ computation-node.c
@@ -36,6 +36,29 @@ void input_callback(const void *data, uint16_t len,
     }
 }
 
+int buildHeader(broadcastMsg msgPrep){
+    int header = 0;
+    header += msgPrep.typeMsg*10000;    //1 = demande de parent; 2 = ?
+    header += msgPrep.rank*100;
+    //reste 2 derniers libres (*1 et *10)
+}
+
+void requestParent(short rank){
+    broadcastMsg msgPrep;
+    msgPrep.rank = rank;
+    msgPrep.typeMsg = 1;
+
+    unsigned int header = buildHeader(msgPrep);    //max 65535
+    nullnet_buf = (uint8_t *)&header;
+    nullnet_len = 2;
+
+    LOG_INFO("Sending Parent");
+    LOG_INFO_LLADDR(NULL);
+    LOG_INFO_("\n");
+
+    NETSTACK_NETWORK.output(NULL);
+}
+
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(nullnet_example_process, ev, data)
 {
@@ -49,27 +72,12 @@ PROCESS_THREAD(nullnet_example_process, ev, data)
     tsch_set_coordinator(linkaddr_cmp(&coordinator_addr, &linkaddr_node_addr));
     #endif /* MAC_CONF_WITH_TSCH */
 
-    //nullnet_len = sizeof(count);
     //nullnet_set_input_callback(input_callback);
     
     etimer_set(&periodic_timer, SEND_INTERVAL);
     while(1) {
         if(parent_Add==-1){
-           /*
-            broadcastMsg msgPrep;
-            msgPrep.rank = rank;
-            msgPrep.typeMsg = 3;
-        */
-           
-            unsigned int header = 12345;    //max 65535
-            nullnet_buf = (uint8_t *)&header;
-            nullnet_len = 2;
-
-            LOG_INFO("Sending Parent");
-            LOG_INFO_LLADDR(NULL);
-            LOG_INFO_("\n");
-
-            NETSTACK_NETWORK.output(NULL);
+            requestParent(rank);
         }else{
             LOG_INFO("Parent != -1\n");
             LOG_INFO_("\n");
