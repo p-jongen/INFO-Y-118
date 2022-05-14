f7af8e2192aece8fb46e69f98e527b792510adc0
Emeric Demeulenere <emericemeu@hotmail.com>
2022-05-14 12:26:25 +0200

    v5.1

--- computation-node.c
+++ computation-node.c
@@ -22,7 +22,7 @@ PROCESS(nullnet_example_process, "NullNet broadcast example");
 AUTOSTART_PROCESSES(&nullnet_example_process);
 
 void sendParentProposal(const linkaddr_t *src){
-    LOG_INFO_("Border : Send Unicast Parent Proposal to ");
+    LOG_INFO_("Computation : Send Unicast Parent Proposal to ");
     LOG_INFO_LLADDR(src);
     LOG_INFO_(" \n");
     broadcastMsg msgPrep;           //prepare info
@@ -36,50 +36,52 @@ void sendParentProposal(const linkaddr_t *src){
     NETSTACK_NETWORK.output(src);
 }
 
+void addParent(const linkaddr_t *src, int parentRankReceived){  //add or update info about his parent
+    parent.address = *src;
+    parent.rank = parentRankReceived;
+    rank = parent.rank+1;
+    parent.hasParent = 1;
+}
+
 /*---------------------------------------------------------------------------*/
-void input_callback(const void *data, uint16_t len,
-                    const linkaddr_t *src, const linkaddr_t *dest)
-{
-    
+void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest){
     int parentRankReceived = 0;
     if(len == sizeof(unsigned)) {
         unsigned bufData;
         memcpy(&bufData, data, 2);
-        int typeMsgReceived = bufData/10000;
+        int typeMsgReceived = bufData/10000; 65655
 
         if(typeMsgReceived == 1){   //Received parent request
             LOG_INFO_("Computation : Receive Parent Request\n");
-            sendParentProposal(src);
+            if (parent.hasParent == 1){                   //n'envoie pas de proposal s'il n'a pas de parent lui-même
+                sendParentProposal(src);
+            }
         }
 
         if(typeMsgReceived == 2){   //received parent proposal
             LOG_INFO_("Computation : Receive Parent Proposal\n");
             parentRankReceived = (bufData/100)%100;
-            if (parent.hasParent ==0){
-                parent.address = *src;
-                parent.rank = parentRankReceived;
-                parent.hasParent = 1;
+            if (parent.hasParent == 0){
+                addParent(src, parentRankReceived);
+
                 LOG_INFO_("New parent for Computation : ");
-                LOG_INFO_LLADDR(src);
-                LOG_INFO_(" (Parent rank = %d)\n", parent.rank);
             }else{  //déjà un parent
                 if( parentRankReceived < parent.rank){
-                    parent.rank = (bufData/100)%100;
-                    parent.address = *src;
+                    addParent(src, parentRankReceived);
+
                     LOG_INFO_("Better parent for Computation :  ");
-                    LOG_INFO_LLADDR(src);
-                    LOG_INFO_(" (Parent rank = %d)\n", parent.rank);
                 }
                 else{
                     LOG_INFO_("Old parent holded for Computation : ");
-                    LOG_INFO_LLADDR(src);
-                    LOG_INFO_(" (Parent rank = %d)\n", parent.rank);
                 }
             }
+            LOG_INFO_LLADDR(src);
+            LOG_INFO_(" (Parent rank = %d)\n", parent.rank);
         }
-    }/*else{
-        LOG_INFO("Computation : Message received but error\n");
-    }*/
+        if(typeMsgReceived == 3){
+
+        }
+    }
 }
 
 void requestParent(short rank){
--- sensor-node.c
+++ sensor-node.c
@@ -1,3 +1,4 @@
+
 #include "contiki.h"
 #include "net/netstack.h"
 #include "net/nullnet/nullnet.h"
@@ -7,37 +8,122 @@
 
 /* Log configuration */
 #include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
 
-PROCESS(sensor_process, "Sensor node");
-AUTOSTART_PROCESSES(&sensor_process);
+/* Configuration */
+#define SEND_INTERVAL (8 * CLOCK_SECOND)
+
+static node_t parent;
+static int rank = 99;
 
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(sensor_process, ev, data)
+PROCESS(nullnet_example_process, "NullNet broadcast example");
+AUTOSTART_PROCESSES(&nullnet_example_process);
+
+void sendParentProposal(const linkaddr_t *src){
+    LOG_INFO_("Computation : Send Unicast Parent Proposal to ");
+    LOG_INFO_LLADDR(src);
+    LOG_INFO_(" \n");
+    broadcastMsg msgPrep;           //prepare info
+    msgPrep.rank = rank;
+    msgPrep.typeMsg = 2;
+
+    unsigned int header = buildHeader(msgPrep);    //build header (node.h)
+    nullnet_buf = (uint8_t *)&header;
+    nullnet_len = 2;
+
+    NETSTACK_NETWORK.output(src);
+}
+
+void addParent(const linkaddr_t *src, int parentRankReceived){  //add or update info about his parent
+    parent.address = *src;
+    parent.rank = parentRankReceived;
+    rank = parent.rank+1;
+    parent.hasParent = 1;
+}
+
+/*---------------------------------------------------------------------------*/
+void input_callback(const void *data, uint16_t len, const linkaddr_t *src, const linkaddr_t *dest){
+    int parentRankReceived = 0;
+    if(len == sizeof(unsigned)) {
+        unsigned bufData;
+        memcpy(&bufData, data, 2);
+        int typeMsgReceived = bufData/10000; 65655
+
+        if(typeMsgReceived == 1){   //Received parent request
+            LOG_INFO_("Sensor : Receive Parent Request\n");
+            if (parent.hasParent == 1){                   //n'envoie pas de proposal s'il n'a pas de parent lui-même
+                sendParentProposal(src);
+            }
+        }
+
+        if(typeMsgReceived == 2){   //received parent proposal
+            LOG_INFO_("Sensor : Receive Parent Proposal\n");
+            parentRankReceived = (bufData/100)%100;
+            if (parent.hasParent == 0){
+                addParent(src, parentRankReceived);
+
+                LOG_INFO_("New parent for Sensor : ");
+            }else{  //déjà un parent
+                if( parentRankReceived < parent.rank){
+                    addParent(src, parentRankReceived);
+
+                    LOG_INFO_("Better parent for Sensor :  ");
+                }
+                else{
+                    LOG_INFO_("Old parent holded for Sensor : ");
+                }
+            }
+            LOG_INFO_LLADDR(src);
+            LOG_INFO_(" (Parent rank = %d)\n", parent.rank);
+        }
+        if(typeMsgReceived == 3){
+
+        }
+    }
+}
+
+void requestParent(short rank){
+    LOG_INFO_("Sensor : Sending Parent Request\n");
+    broadcastMsg msgPrep;                   //prepare info
+    msgPrep.rank = rank;
+    msgPrep.typeMsg = 1;
+
+    unsigned int header = buildHeader(msgPrep);    //(in node.h) -> build header TypeMsg(1)-Rank(23)-vide(45) = 12345
+    nullnet_buf = (uint8_t *)&header;
+    nullnet_len = 2;
+
+    NETSTACK_NETWORK.output(NULL);
+}
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(nullnet_example_process, ev, data)
 {
-  static struct etimer periodic_timer;
-  static unsigned count = 0;
+    static struct etimer periodic_timer;
 
-  PROCESS_BEGIN();
+    PROCESS_BEGIN();
 
-  /* Initialize NullNet */
-  nullnet_buf = (uint8_t *)&count;
-  nullnet_len = sizeof(count);
-  nullnet_set_input_callback(input_callback);
+    nullnet_set_input_callback(input_callback);         //LISTENER
 
-  if(!linkaddr_cmp(&dest_addr, &linkaddr_node_addr)) {
     etimer_set(&periodic_timer, SEND_INTERVAL);
     while(1) {
-      PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
-      LOG_INFO("Sending %u to ", count);
-      LOG_INFO_LLADDR(&dest_addr);
-      LOG_INFO_("\n");
+        //parent
+        if(parent.hasParent == 0){                 //if no parent, send request
+            LOG_INFO_("Sensor parent : None\n");
+            requestParent(rank);
+        }else{
+            LOG_INFO_("Sensor parent : Has a parent");
 
-      NETSTACK_NETWORK.output(&dest_addr);
-      count++;
-      etimer_reset(&periodic_timer);
+        }
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&periodic_timer));
+        etimer_reset(&periodic_timer);
+
+        //sensor Value
+
     }
-  }
 
-  PROCESS_END();
+    //nullnet_set_input_callback(input_callback);
+
+    PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
