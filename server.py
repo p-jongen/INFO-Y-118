c35f34561acbb435276ed392fe213de20568df04
hamze10 <hamzamahmoudi10@gmail.com>
2022-05-15 19:29:06 +0200

    server.py almost done

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import socket
 import argparse
 import sys
 
+threshold = 60
+dict_sensors = dict()
+
 def recv(sock):
     data = sock.recv(1)
     message = b""
@@ -10,14 +13,34 @@ def recv(sock):
         data = sock.recv(1)
     return message
 
+def verify_slope(address):
+    slope = sum(dict_sensors[address]) / 30
+    return "OPEN" if slope > threshold else "CLOSE"
+
 def process(data):
     result = ""
     if data.startswith("[SENSOR_VALUE]"):
-        sensor_value = data.split()[-1]
-        result = "OPEN" #TODO compute to know if open or not
+        splitted = data.split()
+        address = splitted[-1]
+        sensor_value = int(splitted[-2])
+        
+        #create the list if it is a new address
+        if address not in dict_sensors.keys():
+            dict_sensors[address] = list()
+
+        #remove first element to keep 30 elements
+        len_dict_address = len(dict_sensors[address])
+        if len_dict_address == 30:
+            dict_sensors[address].pop(0)
+
+        dict_sensors[address].append(sensor_value)
+
+        #verify the slope only if there is at least 30 values
+        if len_dict_address == 30:
+            result = verify_slope(address)
     return result
 
-def main(ip, port, threshold):
+def main(ip, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((ip, port))
 
@@ -25,17 +48,14 @@ def main(ip, port, threshold):
         data = recv(sock).decode("utf-8")
         result = process(data)
         if result:
-            #TODO send the result to the border in the right format (not a str)
-            #sock.sendall(result)
-            print("")
+            sock.sendall(bytes(result, "utf-8"))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ip", dest="ip", type=str)
     parser.add_argument("--port", dest="port", type=int)
-    parser.add_argument("--threshold", dest="threshold", type=int, default=0)
     args = parser.parse_args()
 
-    main(args.ip, args.port, args.threshold)
+    main(args.ip, args.port)
 	
 	#usage: python3 server.py --ip 172.17.0.2 --port 60001
