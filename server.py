52c8393116089eccb3aa55e2ee179a5d2d6b2413
hamze10 <hamzamahmoudi10@gmail.com>
2022-05-15 12:24:21 +0200

    add server.py

---
+++ server.py
@@ -0,0 +1,35 @@
+import socket
+import argparse
+import sys
+import time
+
+def recv(sock):
+    data = sock.recv(1)
+    message = b""
+    while data.decode("utf-8") != '\n':
+        message += data
+        data = sock.recv(1)
+    return message
+
+def process(data):
+    #TODO we need to filter data and only check if data == something that border want to send to the server
+    print(data)
+
+def main(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((ip, port))
+
+    while True:
+        data = recv(sock).decode("utf-8")
+        process(data)
+        time.sleep(1)
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", dest="ip", type=str)
+    parser.add_argument("--port", dest="port", type=int)
+    args = parser.parse_args()
+    
+    main(args.ip, args.port)
+
